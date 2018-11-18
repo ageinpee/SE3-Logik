66d96d86e46c4d40b7d78cc44b6feffad867e979
Henrik <henrik.peters.98@web.de>
2018-11-18 10:55:43 +0100

    added 1.3

--- SE3LP-G5_A03_KHP.pl
+++ SE3LP-G5_A03_KHP.pl
@@ -20,11 +20,11 @@ anzahl_produkte(KatName, Anz) :- findall(_,
 
 % Bisher verkaufte Exemplare einer Kategorie
 verkaufte_exemplare(KatName, Insgesamt_Verkauft) :- findall(Verkauft,
-                                             (kategorie(KatID, KatName, _),
-                                              produkt(PID, KatID, _, _, _, _, _),
-                                              verkauft(PID, _, _, Verkauft)),
-                                             L),
-                                   sumlist(L, Insgesamt_Verkauft).
+                                                            (kategorie(KatID, KatName, _),
+                                                             produkt(PID, KatID, _, _, _, _, _),
+                                                             verkauft(PID, _, _, Verkauft)),
+                                                            L),
+                                                    sumlist(L, Insgesamt_Verkauft).
 /*
 ?- verkaufte_exemplare(krimi, Insgesamt_Verkauft).
 Insgesamt_Verkauft = 2114.
@@ -32,33 +32,43 @@ Insgesamt_Verkauft = 2114.
 /*========= A1.3*/
 % Berechnet für ein gegebenes Jahr wie stark die Verkaufserlöse durch die
 % Preisnachlässe gegenüber dem vorgegangenem Jahr geschmälert wurden.
-% schmaelerung(Jahr) :-
-
-
+schmaelerung(Jahr1, Insgesamt_Erloesdiff) :- findall(Erloesdiff,
+                                                      (Jahr2 is Jahr1-1,
+                                                        verkauft(PID, Jahr1, Preis1, Anz1),
+                                                        verkauft(PID, Jahr2, Preis2, Anz2),
+                                                        Preis1 < Preis2,
+                                                        Erloesdiff is Anz1*Preis1 - Anz2*Preis2),
+                                                      L),
+                                             sumlist(L, Insgesamt_Erloesdiff).
+/*
+?- schmaelerung(2018, Insgesamt_Erloesdiff).
+Insgesamt_Erloesdiff = 3489.
+--> Bedeutet: Durch den Preisnachlass im Vergleich zum Vorjahr wurden insgesamt 3489 Euro mehr eingenommen.
+*/
 /*========= A2.1 */
 %Abfrage, die für einen Kategorienamen/Teilnamen eine List von ID's zurückgibt, in denen dieser vorkommt.
-search_by_substring(K, Liste) :- findall(KatID , 
-                                          (kategorie(KatID, Name, _), 
-                                            sub_string(Name, _, Length, _, K), Length \= 0), 
+search_by_substring(K, Liste) :- findall(KatID ,
+                                          (kategorie(KatID, Name, _),
+                                            sub_string(Name, _, Length, _, K), Length \= 0),
                                           Liste).
 /*
 ?- verwandte_kategorien(buch, L).
 L = [1, 2, 3, 13, 14, 15, 20, 21].
 */
- 
+
 /*========= A2.2 */
-% Kategorie eines Produktes muss immer in dem Kategoriebaum vorhanden sein. 
+% Kategorie eines Produktes muss immer in dem Kategoriebaum vorhanden sein.
 %=> Prüfe für wo das nciht der Fall ist
 % Bedingung: keine Doppelergebnisse
 % Rückgabe liste der ProduktID's
-produkte_kategorie_nicht_vorhanden(Liste) :- findall(PID, (produkt(PID, KID,_,_,_,_,_), 
+produkte_kategorie_nicht_vorhanden(Liste) :- findall(PID, (produkt(PID, KID,_,_,_,_,_),
                                                           not(kategorie(KID,_,_))), Liste).
 /*
 ?- produkte_kategorie_nicht_vorhanden(Liste).
 Liste = [99999].
 */
 
-/*========= A2.3 
+/*========= A2.3
 Schlüssel und Namen aller Kategorien, die keine gültigen Oberkategorie zugeordnet sind.
 */
 kategorie_obere_kategorie_ungueltig(Liste) :- findall([KID, Name], (kategorie(KID, Name, OID), OID \= 0,
@@ -76,9 +86,9 @@ für die diese Bedingung verletzt ist.
 => gibt Kategorien(ID) aus, für die dieses zutrifft.
 */
 
-mehrere_oberkategorien(L) :- findall(KatID, (kategorie(KatID, _,OID), findall(OID, kategorie(OID,_,_), Sub_Liste), 
-                                            length(Sub_Liste, Laenge), 
-                                            Laenge > 1), 
+mehrere_oberkategorien(L) :- findall(KatID, (kategorie(KatID, _,OID), findall(OID, kategorie(OID,_,_), Sub_Liste),
+                                            length(Sub_Liste, Laenge),
+                                            Laenge > 1),
                                     L).
 /*
 ?- mehrere_oberkategorien(L).
@@ -92,10 +102,10 @@ neueigentuemer(Eigentumer, Strasse, Hausnummer) wahr für:
 => als wahr falsch Frage verstanden.
 */
 neueigentuemer(Eigentuemer, Strasse, Hausnummer) :- bew(_, ONr, _, Eigentuemer, _, Verkaufsdatum),
-                                                    Verkaufsdatum > 20161231, 
-                                                    not(( 
+                                                    Verkaufsdatum > 20161231,
+                                                    not((
                                                       bew(_, ONr, Eigentuemer, _, _, Verkaufsdatum2),
-                                                      Verkaufsdatum2 > Verkaufsdatum)), 
+                                                      Verkaufsdatum2 > Verkaufsdatum)),
                                                     obj(ONr, _, Strasse, Hausnummer, _).
 /*
 ?- neueigentuemer(meier, gaertnerstr, 15).
@@ -109,16 +119,16 @@ vorbesitzer(ObjektID,Besitzer,Vorbesitzer)
 1. mit findall
 2. Reihenfolge -> Rekursion
 */
-vorbesitzer1(ObjektID, Besitzer, Vorbesitzer) :- bew(_,ObjektID,_,Besitzer,_,Datum), 
+vorbesitzer1(ObjektID, Besitzer, Vorbesitzer) :- bew(_,ObjektID,_,Besitzer,_,Datum),
                                                   findall(Verkaufer, (
-                                                      bew(_,ObjektID,Verkaufer,_,_,Datum2), Datum >= Datum2), 
+                                                      bew(_,ObjektID,Verkaufer,_,_,Datum2), Datum >= Datum2),
                                                       Vorbesitzer).
 /*
 ?- vorbesitzer1(3, mueller, Vorbesitzer).
 Vorbesitzer = [schulze, schneider].
 */
 vorbesitzer2(ObjektID, Besitzer, Vorbesitzer) :- bew(_, ObjektID, Vorbesitzer, Besitzer, _, _).
-vorbesitzer2(ObjektID, Besitzer, Vorbesitzer) :- bew(_, ObjektID, Verkaufer, Besitzer, _, _), 
+vorbesitzer2(ObjektID, Besitzer, Vorbesitzer) :- bew(_, ObjektID, Verkaufer, Besitzer, _, _),
                                                   vorbesitzer2(ObjektID, Verkaufer, Vorbesitzer).
 /*
 ?- vorbesitzer2(3, mueller, Vorbesitzer).
@@ -128,7 +138,7 @@ false.
 */
 /*
 Bei der ersten werden direkt im ersten Atemzug alle Vorbesitzer zurückgegeben. Wie man aber gut erkennen kann
-ist die Reihenfolge falsch. Die zweite Funktion hat einen Rekursiven Ansatz. Hier ist die Reihenfolge richtig, 
+ist die Reihenfolge falsch. Die zweite Funktion hat einen Rekursiven Ansatz. Hier ist die Reihenfolge richtig,
 man bekommt aber nicht sofort alle Vorbesitzer geliefert. Je nach Anwendungsfall kann man sich somit eine Variante
 aussuchen.
 */
