45f8e0444ef402262b0cc92b2625e8d7cb34bf5a
Matze <matze@it-dudes.de>
2018-11-30 11:41:40 +0100

    init Ü05 Aufgaben

--- SE3LP-G5_A05_KHP.pl
+++ SE3LP-G5_A05_KHP.pl
@@ -1,7 +1,15 @@
-/* SE3LP-18W-A04
+/* SE3LP-18W-A05
   Namen: Chung-Shan Kao, Harm Matthias Harms, Henrik Peters
 */
 ?- consult('medien2.pl').
 ?- consult('medienKHP.pl').
 ?- consult('hauser.pl').
 ?- consult('galaxy.pl').
+
+/*===== Aufgabe 1*/
+
+/*===== Aufgabe 2*/
+
+/*===== Aufgabe 3*/
+
+/*===== Aufgabe 4*/
