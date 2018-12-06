a6524049829d74864637c79602729a30590a2bc0
Henrik <henrik.peters.98@web.de>
2018-12-06 09:00:10 +0100

    added A06

---
+++ SE3LP-G5_A06_KHP.pl
@@ -0,0 +1,13 @@
+/* SE3LP-18W-A06
+  Namen: Chung-Shan Kao, Harm Matthias Harms, Henrik Peters
+*/
+?- consult('medien2.pl').
+?- consult('medienKHP.pl').
+?- consult('hauser.pl').
+?- consult('galaxy.pl').
+
+/*====================*/
+/*==== Aufgabe 1 =====*/
+/*====================*/
+/*-----Aufgabe 1.1----*/
+%%
