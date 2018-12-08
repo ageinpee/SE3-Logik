b9d4e7671e73b86968bab48395277ba1f475c565
k <7kao@informatik.uni-hamburg.de>
2018-12-08 08:16:59 +0100

    Aufgabe 1.3 und 1.4 sowie Aufgabe 2

--- SE3LP-G5_A06_KHP.pl
+++ SE3LP-G5_A06_KHP.pl
@@ -84,13 +84,171 @@ goldener_schritt_endrek_rec(LastStep, Step, Acc, Result) :-
 /*====================*/
 /*-----Aufgabe 1.3----*/
 
+% fibonacci(+Rekursionstiefe,?Fibonacci-Zahl)
+fibonacci(0,1).
+fibonacci(1,2).
+fibonacci(N,F) :-
+    N > 1,
+    N1 is N - 1, N2 is N - 2,
+    fibonacci(N1,F1), fibonacci(N2,F2),
+    F is F1 + F2.
 
-%%
-%% 
+%% Das gegebene Prädikat ist nicht effizient, weil bei jedem 
+%% Rekursionsschnitt unnötigerweise zwei FIBONACCI-Zahlen berechnet
+%% werden. Jede FIBONACCI-Zahl wird nämlich in zwei konsekutiven 
+%% Rekursionsschritten berechnet. Zudem ist die Rekursion induktiv.
+
+%% Eine bessere Lösung ist es, dass die Rekusion endrekursiv ist und
+%% bei einem Rekursionsschritt nur eine FIBONACCI-Zahl berechnet wird,
+%% während die andere FIBONACI-Zahl vom vorherigen Rekursionssschritt
+%% übergeben wird:
+
+%%%% Prädikat
+%% fibo(+Rekursionstiefe,?Fibonacci-Zahl)
+fibo(N,F) :-
+    fib(N,1,2,F).
+
+fib(0,F1,F2,F) :- F is F1 + F2.
+
+fib(N,F1,F2,F) :-
+    N > 0,
+    N1 is N - 1,
+    F3 is F1 + F2,
+    fib(N1,F2,F3,F).
+    
+/*
+%%%% Testfälle
+%% fibonacci(+Rekursionstiefe,-Fibonacci-Zahl)
+?- time(fibonacci(25,F)).
+485,568 inferences, 1.683 CPU in 1.683 seconds (100% CPU, 288486 Lips)
+F = 196418.
+
+%% fibo(+Rekursionstiefe,-Fibonacci-Zahl)
+? time(fibo(250,F)).
+752 inferences, 0.000 CPU in 0.000 seconds (102% CPU, 3671785 Lips)
+F = 54122222371037658776676579571233761483351206693809497.
+*/
+
+/*====================*/
+/*-----Aufgabe 1.4----*/
+
+%%%% Prädikat
+%% gold_fibonacci(+Rekursionstiefe,?Resultat)
+gold_fibonacci(N,Resultat) :-
+    N1 is N - 1,
+    fibonacci(N1,F1),
+    fibonacci(N, F),
+    Resultat is F/F1.
+
+%%%% Prädikat
+%% gold_fibo(+Rekursionstiefe,?Resultat)
+gold_fibo(N,Resultat) :-
+    N1 is N - 1,
+    fibo(N1,F1),
+    fibo(N, F),
+    Resultat is F/F1.
+
+/*
+%%%% Testfälle
+%% gold_fibonacci(+Rekursionstiefe,-Resultat)
+?- gold_fibonacci(10,Resultat).
+Resultat = 1.6179775280898876.
+
+%% gold_fibo(+Rekursionstiefe,-Resultat)
+? gold_fibo(10,Resultat).
+Resultat = 1.6180257510729614.
+
+%% gold_fibonacci(+Rekursionstiefe,-Resultat)
+?- time(gold_fibonacci(25,Resultat)).
+785,667 inferences, 4.566 CPU in 4.567 seconds (100% CPU, 172052 Lips)
+Resultat = 1.6180339887802426.
+
+%% gold_fibo(+Rekursionstiefe,-Resultat)
+? time(gold_fibo(250,Resultat)).
+1,504 inferences, 0.000 CPU in 0.000 seconds (97% CPU, 4825803 Lips)
+Resultat = 1.6180339887498947.
+
+%% Zu einer gegebenen Rekursionstiefe ist die endkursive Variante 
+%% (neue Definition) ist wesentlich schneller und präziser als die 
+%% induktive Variante (naive Definition).
+*/
+
+/*====================*/
+/*--------Bonus-------*/
+
+%%%% Prädikat
+%% gold_fibo_rdiv(+Rekursionstiefe,?Resultat)
+gold_fibo_rdiv(N,Resultat) :-
+    N1 is N - 1,
+    fibo(N1,F1),
+    fibo(N, F),
+    Resultat is F rdiv F1,
+    format('~50f',[Resultat]).
+    
+/*
+%%%% Testfall
+%% gold_fibo_rdiv(+Rekursionstiefe,-Resultat)
+?- gold_fibo_rdiv(250,Resultat).
+1.61803398874989484820458683436563811772030917980576
+Resultat = 54122222371037658776676579571233761483351206693809497 rdiv 
+           33449372971981195681356806732944396691005381570580873.
+
+%%%% Vergleich mit https://oeis.org/A001622
+%% Die ersten 50 Nachkommazahlen sind identisch.
+1.61803398874989484820458683436563811772030917980576
+*/
 
 /*====================*/
 /*-----Aufgabe 2------*/
 /*====================*/
+
+/*====================*/
+/*-----Aufgabe 2.1----*/
+
+%%%% Prädikat
+%% goldener_schnitt-incr(+Rekursionstiefe,?Resultat)
+goldener_schnitt_incr(N,R) :-    
+    gold_incr(N,R,1).
+
+gold_incr(N,R,Acc) :-
+    N >= 0,
+    R is (1/Acc)+1.
+
+gold_incr(N,R,Acc) :-
+    N > 0,
+    N1 is N-1,
+    R1 is (1/Acc)+1,
+    gold_incr(N1,R,R1).
+    
+/*
+%%%% Testfälle
+%% goldener_schnitt-incr(+Rekursionstiefe,-Resultat)
+?- goldener_schnitt_incr(3,R).
+R = 2;
+R = 1.5;
+R = 1.6666666666666665;
+R = 1.6;
+false.
+
+%% goldener_schnitt-incr(+Rekursionstiefe,+Resultat)
+?- goldener_schnitt_incr(3,1.6).
+true.
+
+%% goldener_schnitt-incr(+Rekursionstiefe,+Resultat)
+?- goldener_schnitt_incr(2,1.6).
+false.
+
+%% goldener_schnitt-incr(+Rekursionstiefe,+Resultat)
+?- goldener_schnitt_incr(3,1.6).
+true.
+*/
+
+/*====================*/
+/*-----Aufgabe 2.2----*/
+
+%% Bereits mit weniger als 10 Rekursionsschritten ist der 
+%% Approximationsfehler nicht mehr zu erkennen.
+
 
 /*====================*/
 /*-----Aufgabe 3------*/
