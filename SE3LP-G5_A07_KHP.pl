a9d5f30ad18a38ec7df99c51b93eea8812b441f8
Henrik Peters <henrik.peters.98@web.de>
2018-12-14 17:41:38 +0100

    added Aufg 1 working now!

--- SE3LP-G5_A07_KHP.pl
+++ SE3LP-G5_A07_KHP.pl
@@ -6,13 +6,21 @@
 /*====================*/
 /*==== Aufgabe 1 =====*/
 
-%% 
-remove_stops(_,[],[]).
-remove_stops(L2,[Element|Tail],R):- member(Element, L2)
-                                   ,!
-                                   ,remove_stops(L2,Tail,R).
-remove_stops(L2,[Element|Tail],[Element|R]):- remove_stops(L2,Tail,R).
+%% del_stop startet und checkt mittels member ob ein Wort in der Stopliste ist.
+%% Wenn ja wird dann mittels ! das Backtracing abgebrochen und es geht mit dem
+%% nächsten Wort weiter. Wenn nicht wird rekursiv weiter gerechnet. Die Rekursion
+%% wird abgebrochen wenn beide Listen leer sind.
+del_stop(_,[],[]).
+del_stop(Stops,[Word|Words],R):- member(Word, Stops),
+                                 !,
+                                 del_stop(Stops, Words, R).
+del_stop(Stops,[Word|Words],[R|Rs]):- del_stop(Stops, Words, Rs).
 
+%% Beispiel:
+%% ?- stop(S), text(1, T), del_stop(S, T, R).
+%% S = [der, die, das, den, dem, des, diese, dieser, diesem|...],
+%% T = [nachdem, er, eine, feuerwerksrakete, von, seinem, hintern, aus, gezündet|...],
+%% R = [_5390, _5396, _5402, _5408, _5414, _5420, _5426, _5432, _5438|...]
 
 /*====================*/
 /*==== Aufgabe 2 =====*/
